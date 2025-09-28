1a62b118cf9207b77977d2e2642dbae248110c5a
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-09-28 14:54:01 +0200

    shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,11 +1,5 @@
 
 
-def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
 
 def main():
     shopping_list = []
@@ -60,3 +54,4 @@ def display_menu():
 if __name__ == "__main__":
 
     main()
+
